418f7815a109e3188cbc7e309bb1ec9e7ab0bc66
Hansana Wickramanayake <hansana@ideabits.se>
2023-05-15 15:17:29 +0530

    hansana COmmit

---
+++ Test Suites/Test Suit two.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suit two</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>98eb9761-771a-4b83-899b-0186565fe92f</testSuiteGuid>
+</TestSuiteEntity>
